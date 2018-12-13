9f024c9dc5c37ea5c760658a7375e9aa3ab5fb7b
Ramon Castan Sutto <ramon.castan@ramons-mbp.br.ibm.com>
2018-12-13 11:34:12 -0200

    messaging

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'mike',
+        text:'Hey, whatup',
+        createdAt:123
+    });
+
+    socket.on('createMessage',(msg) => {
+        console.log('createMessage',msg);
+        
+    })
 
     socket.on('disconnect', () => {
         console.log('client disconnected');
