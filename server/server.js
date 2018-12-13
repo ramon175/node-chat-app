a70febd4f689ad1dbd9eeeabe1512174b52c198d
Ramon Castan Sutto <ramon.castan@ramons-mbp.br.ibm.com>
2018-12-13 10:58:49 -0200

    socket io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const PORT = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT,() => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+        
+    })
+});
+
+
+
+server.listen(PORT,() => {
     console.log(`Server up on port ${PORT}`);
     
 })
