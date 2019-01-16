5ae35b830da9c6a7ea7f8cc66f2b387b03fde60a
Ramon Castan <ramon.castan175@gmail.com>
2019-01-16 21:54:49 -0200

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+    //Selectors
+    var messages = jQuery("#messages");
+    var newMessage = messages.children('li:last-child')
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+        
+    }
+}
+
 socket.on('connect', function() {
     console.log('connected to server');
 });
@@ -21,6 +38,8 @@ socket.on('newMessage', function(message) {
     })
 
     jQuery('#messages').append(html);
+
+    scrollToBottom();
     
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`)
@@ -50,6 +69,7 @@ socket.on('newLocationMessage', function(message){
 
     jQuery('#messages').append(html);
 
+    scrollToBottom();
 })
 
 
