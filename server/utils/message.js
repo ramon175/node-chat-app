3c864db88b45c51ad889df1cd46790f54686fcb8
Ramon Castan <ramon.castan175@gmail.com>
2019-01-16 21:04:08 -0200

    moment timestamp

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,9 +1,11 @@
+const moment = require('moment');
+
 const generateMessage = (from, text) =>{
 
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 
 };
@@ -14,7 +16,7 @@ const generateLocationMessage = (from, latitude, longitude) => {
 
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
         
     };
 
